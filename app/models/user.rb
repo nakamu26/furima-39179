e51c6d4793b0105c20d6972b69ca8eb1768482fd
nakamu26 <n.takanohs@gmail.com>
2023-05-31 21:15:29 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   NAME_KANA_REGEX = /\A[ァ-ヶー]+\z/.freeze
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters" }
-  validates :first_name,      presence: true, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters" }
-  validates :last_name_kana,  presence: true, format: { with: NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters" }
-  validates :first_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name,       presence: true,
+                              format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters' }
+  validates :first_name,      presence: true,
+                              format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters' }
+  validates :last_name_kana,  presence: true,
+                              format: { with: NAME_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: NAME_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
   validates :birth_date,      presence: true
-  validates :password,                        format: { with: PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
-
+  validates :password,
+            format: { with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,69 +5,68 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  ROMAJI = Gimei.first.romaji
-  ZENKAKU_ROMAJI = ROMAJI.tr('0-9a-zA-Z','０-９ａ-ｚＡ-Ｚ')
-  HANKAKU_KATAKANA = NKF.nkf('-w -Z4', Gimei.first.katakana)
-  HIRAGANA = Gimei.first.hiragana
-  KANJI = Gimei.first.kanji
+  romaji = Gimei.first.romaji
+  zenkaku_romaji = romaji.tr('0-9a-zA-Z', '０-９ａ-ｚＡ-Ｚ')
+  hankaku_katakana = NKF.nkf('-w -Z4', Gimei.first.katakana)
+  hiragana = Gimei.first.hiragana
+  kanji = Gimei.first.kanji
 
-  FIRST_NAME_ERR = 'First name is invalid. Input full-width characters'
-  LAST_NAME_ERR = 'Last name is invalid. Input full-width characters'
-  FIRST_NAME_KANA_ERR = 'First name kana is invalid. Input full-width katakana characters'
-  LAST_NAME_KANA_ERR = 'Last name kana is invalid. Input full-width katakana characters'
-
+  first_name_err = 'First name is invalid. Input full-width characters'
+  last_name_err = 'Last name is invalid. Input full-width characters'
+  first_name_kana_err = 'First name kana is invalid. Input full-width katakana characters'
+  last_name_kana_err = 'Last name kana is invalid. Input full-width katakana characters'
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nickname、email、password、password_confirmation、first_name、last_name、
-          first_name_kana、last_name_kana、birth_dateが存在すれば登録できる" do
+      it 'nickname、email、password、password_confirmation、first_name、last_name、
+          first_name_kana、last_name_kana、birth_dateが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
 
     context '新規登録できない場合' do
-      it "nicknameが空では登録できない" do
+      it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      it "emailが空では登録できない" do
+      it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "first_nameが空では登録できない" do
+      it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
-      it "last_nameが空では登録できない" do
+      it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
-      it "first_name_kanaが空では登録できない" do
+      it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
-      it "last_name_kanaが空では登録できない" do
+      it 'last_name_kanaが空では登録できない' do
         @user.last_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
-      it "birth_dateが空では登録できない" do
+      it 'birth_dateが空では登録できない' do
         @user.birth_date = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birth date can't be blank")
       end
 
-      #　email
+      # email
       it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
@@ -80,7 +79,7 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
-      #　password
+      # password
       it 'passwordが5文字以下では登録できない' do
         @user.password = '12345'
         @user.password_confirmation = '12345'
@@ -88,8 +87,8 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが129文字以上では登録できない' do
-        @user.password =  Faker::Lorem.characters(number: 129, min_alpha: 1, min_numeric: 1)
-        @user.password_confirmation =  @user.password
+        @user.password = Faker::Lorem.characters(number: 129, min_alpha: 1, min_numeric: 1)
+        @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
@@ -100,92 +99,92 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
-      #　first_name
+      # first_name
       it 'first_nameが半角英字では登録できない' do
-        @user.first_name = ROMAJI
+        @user.first_name = romaji
         @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_ERR)
+        expect(@user.errors.full_messages).to include(first_name_err)
       end
       it 'first_nameが全角英字では登録できない' do
-        @user.first_name = ZENKAKU_ROMAJI
+        @user.first_name = zenkaku_romaji
         @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_ERR)
+        expect(@user.errors.full_messages).to include(first_name_err)
       end
       it 'first_nameが半角カタカナでは登録できない' do
-        @user.first_name = HANKAKU_KATAKANA
+        @user.first_name = hankaku_katakana
         @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_ERR)
+        expect(@user.errors.full_messages).to include(first_name_err)
       end
 
-      #　last_name
+      # last_name
       it 'last_nameが半角英字では登録できない' do
-        @user.last_name = ROMAJI
+        @user.last_name = romaji
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_ERR)
+        expect(@user.errors.full_messages).to include(last_name_err)
       end
       it 'last_nameが全角英字では登録できない' do
-        @user.last_name = ZENKAKU_ROMAJI
+        @user.last_name = zenkaku_romaji
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_ERR)
+        expect(@user.errors.full_messages).to include(last_name_err)
       end
       it 'last_nameが半角カタカナでは登録できない' do
-        @user.last_name = HANKAKU_KATAKANA
+        @user.last_name = hankaku_katakana
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_ERR)
-      end
-      
-      #　first_name_kana
-      it 'first_name_kanaが半角英字では登録できない' do
-        @user.first_name_kana = ROMAJI
-        @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_KANA_ERR)
-      end
-      it 'first_name_kanaが全角英字では登録できない' do
-        @user.first_name_kana = ZENKAKU_ROMAJI
-        @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_KANA_ERR)
-      end
-      it 'first_name_kanaが半角カタカナでは登録できない' do
-        @user.first_name_kana = HANKAKU_KATAKANA
-        @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_KANA_ERR)
-      end
-      it 'first_name_kanaがひらがなでは登録できない' do
-        @user.first_name_kana = HIRAGANA
-        @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_KANA_ERR)
-      end
-      it 'first_name_kanaが漢字では登録できない' do
-        @user.first_name_kana = KANJI
-        @user.valid?
-        expect(@user.errors.full_messages).to include(FIRST_NAME_KANA_ERR)
+        expect(@user.errors.full_messages).to include(last_name_err)
       end
 
-      #　last_name_kana
-      it 'last_name_kanaが半角英字では登録できない' do
-        @user.last_name_kana = ROMAJI
+      # first_name_kana
+      it 'first_name_kanaが半角英字では登録できない' do
+        @user.first_name_kana = romaji
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_KANA_ERR)
+        expect(@user.errors.full_messages).to include(first_name_kana_err)
+      end
+      it 'first_name_kanaが全角英字では登録できない' do
+        @user.first_name_kana = zenkaku_romaji
+        @user.valid?
+        expect(@user.errors.full_messages).to include(first_name_kana_err)
+      end
+      it 'first_name_kanaが半角カタカナでは登録できない' do
+        @user.first_name_kana = hankaku_katakana
+        @user.valid?
+        expect(@user.errors.full_messages).to include(first_name_kana_err)
+      end
+      it 'first_name_kanaがひらがなでは登録できない' do
+        @user.first_name_kana = hiragana
+        @user.valid?
+        expect(@user.errors.full_messages).to include(first_name_kana_err)
+      end
+      it 'first_name_kanaが漢字では登録できない' do
+        @user.first_name_kana = kanji
+        @user.valid?
+        expect(@user.errors.full_messages).to include(first_name_kana_err)
+      end
+
+      # last_name_kana
+      it 'last_name_kanaが半角英字では登録できない' do
+        @user.last_name_kana = romaji
+        @user.valid?
+        expect(@user.errors.full_messages).to include(last_name_kana_err)
       end
       it 'last_name_kanaが全角英字では登録できない' do
-        @user.last_name_kana = ZENKAKU_ROMAJI
+        @user.last_name_kana = zenkaku_romaji
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_KANA_ERR)
+        expect(@user.errors.full_messages).to include(last_name_kana_err)
       end
       it 'last_name_kanaが半角カタカナでは登録できない' do
-        @user.last_name_kana = HANKAKU_KATAKANA
+        @user.last_name_kana = hankaku_katakana
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_KANA_ERR)
+        expect(@user.errors.full_messages).to include(last_name_kana_err)
       end
       it 'last_name_kanaがひらがなでは登録できない' do
-        @user.last_name_kana = HIRAGANA
+        @user.last_name_kana = hiragana
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_KANA_ERR)
+        expect(@user.errors.full_messages).to include(last_name_kana_err)
       end
       it 'last_name_kanaが漢字では登録できない' do
-        @user.last_name_kana = KANJI
+        @user.last_name_kana = kanji
         @user.valid?
-        expect(@user.errors.full_messages).to include(LAST_NAME_KANA_ERR)
+        expect(@user.errors.full_messages).to include(last_name_kana_err)
       end
     end
   end
