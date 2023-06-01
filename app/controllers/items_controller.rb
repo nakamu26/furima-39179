2427f5f7ea3869b3390cc656414a922f428bcce7
nakamu26 <n.takanohs@gmail.com>
2023-06-01 21:48:23 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :move_to_top_page, only: [:new, :create]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id,
+                                 :shipping_fee_status_id, :prefecture_id,
+                                 :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_top_page
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
