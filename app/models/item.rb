9af9fb0d7d36eddb9ccab7f89e75b8b1c088ffff
nakamu26 <n.takanohs@gmail.com>
2023-06-02 18:14:04 +0900

    エラーハンドリングのためのバリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -11,10 +12,12 @@ class Item < ApplicationRecord
   validates :image,                  attached_file_presence: true
   validates :name,                   presence: true
   validates :description,            presence: true
-  validates :category_id,            numericality: { other_than: 0 }
-  validates :condition_id,           numericality: { other_than: 0 }
-  validates :shipping_fee_status_id, numericality: { other_than: 0 }
-  validates :prefecture_id,          numericality: { other_than: 0 }
-  validates :scheduled_delivery_id,  numericality: { other_than: 0 }
-  validates :price,                  presence: true
+  validates :category_id,            numericality: { other_than: 0, message: "can't be blank" }
+  validates :condition_id,           numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id,          numericality: { other_than: 0, message: "can't be blank" }
+  validates :scheduled_delivery_id,  numericality: { other_than: 0, message: "can't be blank" }
+  validates :price,                  presence: true,
+                                     inclusion: { in: 300..9_999_999, message: 'is out of setting range' },
+                                     numericality: { message: 'is invalid. Input half-width characters' }
 end
