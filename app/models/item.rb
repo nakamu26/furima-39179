a0823d02e357f3f65d31fc4c213de07a37f02316
nakamu26 <n.takanohs@gmail.com>
2023-06-03 21:32:35 +0900

    バリデーションの記述をリファクタリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,16 +10,19 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
   HALF_WIDTH_NUMBER = /\A[0-9]+\z/.freeze
-  validates :image,                  attached_file_presence: true
-  validates :name,                   presence: true
-  validates :description,            presence: true
-  validates :category_id,            numericality: { other_than: 0, message: "can't be blank" }
-  validates :condition_id,           numericality: { other_than: 0, message: "can't be blank" }
-  validates :shipping_fee_status_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :prefecture_id,          numericality: { other_than: 0, message: "can't be blank" }
-  validates :scheduled_delivery_id,  numericality: { other_than: 0, message: "can't be blank" }
-  validates :price_before_type_cast, presence: true,
-                                     format: { with: HALF_WIDTH_NUMBER, message: 'is invalid. Input half-width characters' },
-                                     numericality: { only_integer: true, greater_than_or_equal_to: 300,
-                                                     less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :image, attached_file_presence: true
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price_before_type_cast, format: { with: HALF_WIDTH_NUMBER, message: 'is invalid. Input half-width characters' },
+                                       numericality: { only_integer: true, greater_than_or_equal_to: 300,
+                                                       less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  end
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
 end
