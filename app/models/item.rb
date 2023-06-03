7f1a39687ed53e93fe6dfe07dc505126c23ea38f
nakamu26 <n.takanohs@gmail.com>
2023-06-03 20:43:34 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
+  HALF_WIDTH_NUMBER = /\A[0-9]+\z/.freeze
   validates :image,                  attached_file_presence: true
   validates :name,                   presence: true
   validates :description,            presence: true
@@ -17,7 +18,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :prefecture_id,          numericality: { other_than: 0, message: "can't be blank" }
   validates :scheduled_delivery_id,  numericality: { other_than: 0, message: "can't be blank" }
-  validates :price,                  presence: true,
-                                     inclusion: { in: 300..9_999_999, message: 'is out of setting range' },
-                                     numericality: { message: 'is invalid. Input half-width characters' }
+  validates :price_before_type_cast, presence: true,
+                                     format: { with: HALF_WIDTH_NUMBER, message: 'is invalid. Input half-width characters' },
+                                     numericality: { only_integer: true, greater_than_or_equal_to: 300,
+                                                     less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
