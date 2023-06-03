c191145c3fea93b094fabfc6b2afa3f76dd28a46
nakamu26 <n.takanohs@gmail.com>
2023-06-03 18:17:50 +0900

    ログイン分岐修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_top_page, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
@@ -23,9 +23,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id,
                                  :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_top_page
-    redirect_to root_path unless user_signed_in?
   end
 end
