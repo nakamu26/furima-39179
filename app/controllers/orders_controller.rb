fdadd3d6cbdeba1b04bd0d7297a39c4c71b170c1
nakamu26 <n.takanohs@gmail.com>
2023-06-13 20:33:21 +0900

    ordersコントローラにnewおよびcreateアクション定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,29 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:new, :create]
+
   def new
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
+          .merge(user_id: current_user.id).merge(item_id: params[:item_id])
   end
 end
