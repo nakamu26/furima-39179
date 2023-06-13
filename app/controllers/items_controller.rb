99c907c2071dc930a82d9fce9b067e54e8ecaebb
nakamu26 <n.takanohs@gmail.com>
2023-06-13 22:31:16 +0900

    商品一覧のSoldOut分岐を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show, :new, :create]
+
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
+    @orders = Order.all
   end
 
   def new
