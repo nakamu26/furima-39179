7d9744e6003827cea9e2286a6d258f906f7ee309
nakamu26 <n.takanohs@gmail.com>
2023-06-14 19:50:26 +0900

    orderコントローラのnewアクションをindexアクションに修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :move_to_index, only: [:new, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
-  before_action :set_item, only: [:new, :create]
+  before_action :set_item, only: [:index, :create]
 
-  def new
+  def index
     @order_address = OrderAddress.new
   end
 
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
